00fa27703da3e20e467c85739c4531a21fe6e219
Riya Romanova <riyaromanova13@gmail.com>
2020-11-28 01:56:58 +0300

    feat(script.js): have questions

--- js/script.js
+++ js/script.js
@@ -6,32 +6,38 @@ function getRandomInt() {
     return Math.floor(Math.random() * (max - min + 1 )) + min; //получение случайного числа в заданном интервале
 }
 
-// const botNum = getRandomInt();
-
-function startBot(number) { 
-    let userNum = prompt('Угадай число от 1 до 100');
-
+function startBot(number, attempt) { 
+    let res = true;
+    let userNum;
+    if (res === true) {
+        userNum = prompt('Угадай число от 1 до 100');
+    }
     return function guessNum() {
-        if ( userNum === null) {
+        if (attempt === 1) {
+            attempt--;
+            res = confirm("Попытки закончились, хотите сыграть еще?");
+        }
+        if ( userNum === null || res === false) {
             alert("Игра окончена");
         } else {
-            if (Number.isInteger(+userNum) !== true) {
+            if (Number.isInteger(+userNum) !== true && attempt > 0) {
                 userNum = prompt("Введи число!");
                 return(guessNum());
-            } else if (+userNum > number) {
-                userNum = prompt("Загаданное число меньше");
+            } else if (+userNum > number && attempt > 0) {
+                attempt--;
+                userNum = prompt("Загаданное число меньше, осталось попыток " + attempt);
                 return(guessNum());
-            } else if (+userNum < number) {
-                userNum = prompt("Загаданное число больше");
+            } else if (+userNum < number && attempt > 0) {
+                attempt--;
+                userNum = prompt("Загаданное число больше, осталось попыток " + attempt);
                 return(guessNum());
-            } else if (+userNum === number) {
-                alert("Поздравляю, Вы угадали!");
-            } 
+            } else if (+userNum === number && attempt > 0) {
+                res = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+            }
         }
-    
     };
 }
-const bot = startBot(getRandomInt());
+const bot = startBot(getRandomInt(), 10);
 bot();
 
     
